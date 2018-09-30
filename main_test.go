f329dcf404c911ffb49dfb7333d0edb6a8ef7b75
wujunjian <wujunjian@didichuxing.com>
2018-09-30 18:54:21 +0800

    add extenddata

--- main_test.go
+++ main_test.go
@@ -1,46 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"testing"
 	"time"
 
 	"./geohash"
 	"./toll"
+	"go.intra.xiaojukeji.com/gulfstream/plutus/common/helpers"
 )
 
-func TestGetTolls(t *testing.T) {
-	signedtolls := toll.GetTolls(-37.82613500, 144.96554400)
-	if signedtolls != nil {
-		for _, signedtoll := range signedtolls {
-			fmt.Printf("site=%d||tollid=%d||box=[%.8f,%.8f;%.8f,%.8f;%.8f,%.8f;%.8f,%.8f]\n",
-				signedtoll.Site,
-				signedtoll.Toll.Id,
-				signedtoll.Box.MaxLng, signedtoll.Box.MaxLat,
-				signedtoll.Box.MinLng, signedtoll.Box.MaxLat,
-				signedtoll.Box.MaxLng, signedtoll.Box.MinLat,
-				signedtoll.Box.MinLng, signedtoll.Box.MinLat)
+func parseJson(file string) []*helpers.Point {
+	type Cood struct {
+		Lat float64 `json:"lat,string"`
+		Lng float64 `json:"lng,string"`
+	}
+	type OriData struct {
+		Data []*Cood `json:"data"`
+	}
+
+	b, _ := ioutil.ReadFile(file)
+	d := &OriData{}
+	json.Unmarshal(b, d)
+
+	points := []*helpers.Point{}
+	for _, c := range d.Data {
+		p := &helpers.Point{
+			Lat: c.Lat,
+			Lng: c.Lng,
 		}
+		points = append(points, p)
+	}
+	return points
+}
+
+func printpoint(points []*helpers.Point) {
+	for _, p := range points {
+		fmt.Printf("%.8f,%.8f;", p.Lng, p.Lat)
 	}
 }
 
+func TestMatch(t *testing.T) {
+	//init需要一些时间100-200ms左右.
+	time.Sleep(500 * time.Millisecond)
+	points := parseJson("./track_1.json")
+	begin := time.Now()
+	tm := &toll.TollsMatched{}
+	tm.Match(points, "AU")
+	tm.DebugPrint()
+	fmt.Println(time.Now().Sub(begin))
+	printpoint(points)
+}
+
+func TestGetTolls(t *testing.T) {
+
+	//init需要一些时间, 100-200ms左右.
+	time.Sleep(500 * time.Millisecond)
+
+	points := parseJson("./track_1.json")
+	begin := time.Now()
+	for _, p := range points {
+		signedtolls := toll.GetTolls(p.Lat, p.Lng, "AU")
+		if signedtolls != nil {
+			for _, signedtoll := range signedtolls {
+				fmt.Printf("site=%s||tollid=%d||box=[%.8f,%.8f;%.8f,%.8f;%.8f,%.8f;%.8f,%.8f]\n",
+					signedtoll.SiteString(),
+					signedtoll.Toll.Id,
+					signedtoll.Box.MaxLng, signedtoll.Box.MaxLat,
+					signedtoll.Box.MinLng, signedtoll.Box.MaxLat,
+					signedtoll.Box.MaxLng, signedtoll.Box.MinLat,
+					signedtoll.Box.MinLng, signedtoll.Box.MinLat)
+			}
+		}
+	}
+	fmt.Println(time.Now().Sub(begin))
+}
+
+//获取收费站信息
 func TestDebug(t *testing.T) {
 
-	//geohash.Debug(-37.82613500,144.96554400, 8)
-	//return
-	starttime := time.Now()
-
-	spend := time.Now().Sub(starttime)
-	fmt.Println(spend)
-
-	toll.Debug(110, 0)
+	begin := time.Now()
+	//toll.Debug(111, 1, "AU")
+	//toll.Debug(111, 0, "AU")
+	toll.Debug(1, -1, "AU")
+	fmt.Println(time.Now().Sub(begin))
 }
 
 func TestEncode(t *testing.T) {
 
 	var box *geohash.Box
-	var precision float64 = 0.01 //精度的上限,格子不能全部超出该范围 0.01(889-1113m)  0.001(88.9-111.3m)
-	var hashprecision int = 6    //精度的下线,一个格子代表的面积    6(1223*488)  7(150*120)
+	var precision = 0.01  //精度的上限,格子不能全部超出该范围 0.01(889-1113m)  0.001(88.9-111.3m)
+	var hashprecision = 6 //精度的下线,一个格子代表的面积    6(1223*488)  7(150*120)
 
 	mylat := -37.82496
 	mylon := 144.98083
--- toll/toll.go
+++ toll/toll.go
@@ -3,46 +3,108 @@ package toll
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 	"sync"
 	"time"
 
 	"io/ioutil"
 	"path"
+
 	"../geohash"
+	"go.intra.xiaojukeji.com/gulfstream/plutus/common/helpers"
 )
 
 const (
-	TOLL_CLOSE_TO_POINT   = 100
-	TOLL_CLOSE_TO_STATION = 1000
+	//TOLL_CLOSE_TO_POINT   = 100
+	//TOLL_CLOSE_TO_STATION = 1000
 
-	//TODO 这俩个参数需要可配置
-	precision     float64 = 0.001 //精度的上限,格子不能全部超出该范围 0.01(889-1113m)  0.001(88.9-111.3m)
-	hashprecision int     = 8     //精度的下限,一个格子代表的面积    6(1223*488m)  7(150*120m) 8(±0.019km)
+	//需要可配置的参数
+	precision          float64 = 0.0012 //精度的上限,格子不能全部超出该范围 0.01(889-1113m)  0.001(88.9-111.3m)
+	hashprecision      int     = 8      //精度的下限,一个格子代表的面积    6(1223*488m)  7(150*120m) 8(±0.019km)
+	reloadtimeinterval         = 1800   //配置文件reload的时间间隔,单位秒
 
+	//不需要配置的参数
 	before  = -1
 	station = 0
 	after   = 1
 )
 
-type OriTollData struct {
-	Tolls []OriToll `json:"tolls"`
+type matched struct {
+	before  int
+	station int
+	after   int
 }
 
-type OriToll struct {
-	Id                      int     `json:"id,string"`
-	ExternalId              string  `json:"externalId"`
-	Name                    string  `json:"name"`
-	TollLatitude            float64 `json:"tollLatitude,string"`
-	TollLongitude           float64 `json:"tollLongitude,string"`
-	BeforeLocationLatitude  float64 `json:"beforeLocationLatitude,string"`
-	BeforeLocationLongitude float64 `json:"beforeLocationLongitude,string"`
-	AfterLocationLatitude   float64 `json:"afterLocationLatitude,string"`
-	AfterLocationLongitude  float64 `json:"afterLocationLongitude,string"`
-	Heading                 string  `json:"heading"`
-	Price                   float64 `json:"price,string"`
-	LastUpdate              string  `json:"lastUpdate"`
+//TollsMatched key:Toll id
+type TollsMatched struct {
+	toll map[int]*matched
 }
 
+//DebugPrint message
+func (t *TollsMatched) DebugPrint() {
+	for id, m := range t.toll {
+		fmt.Printf("id=%d||beforematched=%d||stationmatched=%d||aftermatched=%d\n", id, m.before, m.station, m.after)
+	}
+}
+
+//Match all posible tolls
+func (t *TollsMatched) Match(points []*helpers.Point, country string) {
+	t.toll = make(map[int]*matched)
+	for _, p := range points {
+		signedtolls := GetTolls(p.Lat, p.Lng, country)
+		if signedtolls != nil {
+			for _, signedtoll := range signedtolls {
+
+				id := signedtoll.Toll.Id
+				m, ok := t.toll[id]
+				if !ok {
+					m = &matched{}
+					t.toll[id] = m
+				}
+				switch signedtoll.Site {
+				case before:
+					m.before++
+				case station:
+					m.station++
+				case after:
+					m.after++
+				}
+			}
+		}
+	}
+}
+
+type oriTollData struct {
+	Tolls []oriToll `json:"tolls"`
+}
+
+type tPoint struct {
+	Lat float64 `json:"lat,string"`
+	Lng float64 `json:"lng,string"`
+}
+
+type oriToll struct {
+	Id                      int      `json:"id,string"`
+	ExternalId              string   `json:"externalId"`
+	Name                    string   `json:"name"`
+	TollLatitude            float64  `json:"tollLatitude,string"`
+	TollLongitude           float64  `json:"tollLongitude,string"`
+	ExtendStation           []tPoint `json:"extendStation,[]interface{}"`
+	DelStation              []tPoint `json:"delStation,[]interface{}"`
+	BeforeLocationLatitude  float64  `json:"beforeLocationLatitude,string"`
+	BeforeLocationLongitude float64  `json:"beforeLocationLongitude,string"`
+	ExtendBefore            []tPoint `json:"extendBefore,[]interface{}"`
+	DelBefore               []tPoint `json:"delBefore,[]interface{}"`
+	AfterLocationLatitude   float64  `json:"afterLocationLatitude,string"`
+	AfterLocationLongitude  float64  `json:"afterLocationLongitude,string"`
+	ExtendAfter             []tPoint `json:"extendAfter,[]interface{}"`
+	DelAfter                []tPoint `json:"delAfter,[]interface{}"`
+	Heading                 string   `json:"heading"`
+	Price                   float64  `json:"price,string"`
+	LastUpdate              string   `json:"lastUpdate"`
+}
+
+// Toll info
 type Toll struct {
 	Id           int            `json:"id"`
 	BeforPoint   *helpers.Point `json:"befor_location"`
@@ -51,6 +113,7 @@ type Toll struct {
 	Price        float64        `json:"price"`
 }
 
+// SignedToll 命中的收费站
 type SignedToll struct {
 	Toll *Toll
 
@@ -58,9 +121,10 @@ type SignedToll struct {
 	Site int
 	Box  *geohash.Box
 }
+type geoHashMap map[string][]*SignedToll
 
 var globalGeolock *sync.RWMutex
-var globalGeoHashMap map[string][]*SignedToll
+var countryMap map[string]geoHashMap
 
 func init() {
 	globalGeolock = &sync.RWMutex{}
@@ -68,18 +132,18 @@ func init() {
 	updateGlobalGeoHashMap := func() {
 		for {
 			globalGeolock.Lock()
-			globalGeoHashMap = make(map[string][]*SignedToll)
+			countryMap = make(map[string]geoHashMap)
 			LoadBrTolls("./conf/tolls")
 			globalGeolock.Unlock()
 
-			time.Sleep(30* time.Second)
+			time.Sleep(reloadtimeinterval * time.Second)
 		}
 
 	}
 	go updateGlobalGeoHashMap()
 }
 
-func (s *SignedToll) siteString() string {
+func (s *SignedToll) SiteString() string {
 	switch s.Site {
 	case before:
 		return "b"
@@ -103,30 +167,41 @@ func (s *SignedToll) point() (float64, float64) {
 	return 0, 0
 }
 
-func Debug(tollid, site int) {
+//Debug Print globalGeoHashMap
+func Debug(tollid, site int, where string) {
 	globalGeolock.RLock()
 	defer globalGeolock.RUnlock()
 
-	for geohash, signedtolls := range globalGeoHashMap {
-		for _, signedtoll := range signedtolls {
-			if (tollid == signedtoll.Toll.Id && site == signedtoll.Site) || tollid == 0 {
-				lat, lng := signedtoll.point()
-				fmt.Printf("geohash=%s||site=%s||tollId=%03d||lng&lat=%.8f,%.8f||box=[%.8f,%.8f;%.8f,%.8f;%.8f,%.8f;%.8f,%.8f]\n",
-					geohash, signedtoll.siteString(), signedtoll.Toll.Id, lng, lat,
-					signedtoll.Box.MaxLng, signedtoll.Box.MaxLat,
-					signedtoll.Box.MinLng, signedtoll.Box.MaxLat,
-					signedtoll.Box.MaxLng, signedtoll.Box.MinLat,
-					signedtoll.Box.MinLng, signedtoll.Box.MinLat)
+	for country, geohashmap := range countryMap {
+		for geohash, signedtolls := range geohashmap {
+			for _, signedtoll := range signedtolls {
+				if (tollid == signedtoll.Toll.Id && site == signedtoll.Site && country == where) || tollid == 0 {
+					lat, lng := signedtoll.point()
+					fmt.Printf("country=%s||geohash=%s||site=%s||tollId=%03d||lng&lat=%.8f,%.8f||box=[%.8f,%.8f;%.8f,%.8f;%.8f,%.8f;%.8f,%.8f]\n",
+						country, geohash, signedtoll.SiteString(), signedtoll.Toll.Id, lng, lat,
+						signedtoll.Box.MaxLng, signedtoll.Box.MaxLat,
+						signedtoll.Box.MinLng, signedtoll.Box.MaxLat,
+						signedtoll.Box.MaxLng, signedtoll.Box.MinLat,
+						signedtoll.Box.MinLng, signedtoll.Box.MinLat)
+				}
 			}
 		}
 	}
+
 }
 
-func GetTolls(latitude, longitude float64) []*SignedToll {
+//GetTolls Get hit Tolls
+func GetTolls(latitude, longitude float64, country string) []*SignedToll {
 	hash, _ := geohash.Encode(latitude, longitude, hashprecision)
 
 	globalGeolock.RLock()
 	defer globalGeolock.RUnlock()
+
+	globalGeoHashMap, ok := countryMap[country]
+	if !ok {
+		return nil
+	}
+
 	v, ok := globalGeoHashMap[hash]
 	if !ok {
 		return nil
@@ -134,6 +209,7 @@ func GetTolls(latitude, longitude float64) []*SignedToll {
 	return v
 }
 
+// LoadBrTolls read files and loadBrTolls
 func LoadBrTolls(tollsFile string) error {
 	if tollsFile == "" {
 		return nil
@@ -146,32 +222,97 @@ func LoadBrTolls(tollsFile string) error {
 		if f.IsDir() {
 			continue
 		}
-		fileBytes, err := ioutil.ReadFile(path.Join(tollsFile, f.Name()))
+
+		filename := f.Name()
+		country := strings.Split(f.Name(), ".")[0]
+		fileBytes, err := ioutil.ReadFile(path.Join(tollsFile, filename))
 		if err != nil {
-			return fmt.Errorf("readfile failed, file=%v err=%v", f.Name(), err)
+			return fmt.Errorf("readfile failed, file=%v err=%v", filename, err)
 		}
 
-		data := OriTollData{}
+		data := oriTollData{}
 		if err = json.Unmarshal(fileBytes, &data); err != nil {
-			return fmt.Errorf("unmarshal file failed, file=%v err=%v", f.Name(), err)
+			return fmt.Errorf("unmarshal file failed, file=%v err=%v", filename, err)
 		}
 
 		for _, oriToll := range data.Tolls {
 			Toll := oriToll.ToToll()
 
-			Tomap(oriToll.BeforeLocationLatitude, oriToll.BeforeLocationLongitude, Toll, before)
+			var tp tPoint
+			for _, tp = range oriToll.ExtendBefore {
+				narrowtomap(tp.Lat, tp.Lng, Toll, before, country)
+			}
+			tomap(oriToll.BeforeLocationLatitude, oriToll.BeforeLocationLongitude, Toll, before, country)
+			for _, tp = range oriToll.DelBefore {
+				narrowdelmap(tp.Lat, tp.Lng, Toll, before, country)
+			}
 
-			Tomap(oriToll.TollLatitude, oriToll.TollLongitude, Toll, station)
+			for _, tp = range oriToll.ExtendStation {
+				narrowtomap(tp.Lat, tp.Lng, Toll, station, country)
+			}
+			tomap(oriToll.TollLatitude, oriToll.TollLongitude, Toll, station, country)
+			for _, tp = range oriToll.DelStation {
+				narrowdelmap(tp.Lat, tp.Lng, Toll, station, country)
+			}
 
-			Tomap(oriToll.AfterLocationLatitude, oriToll.AfterLocationLongitude, Toll, after)
+			for _, tp = range oriToll.ExtendAfter {
+				narrowtomap(tp.Lat, tp.Lng, Toll, after, country)
+			}
+			tomap(oriToll.AfterLocationLatitude, oriToll.AfterLocationLongitude, Toll, after, country)
+			for _, tp = range oriToll.DelAfter {
+				narrowdelmap(tp.Lat, tp.Lng, Toll, after, country)
+			}
 		}
 	}
 	return nil
 }
 
-func Tomap(latitude, longitude float64, toll *Toll, site int) {
+func narrowdelmap(latitude, longitude float64, toll *Toll, site int, country string) {
+	fmt.Println("delete:", latitude, longitude, site, country)
+	h, _ := geohash.Encode(latitude, longitude, hashprecision)
+	hashmap, ok := countryMap[country]
+	if !ok {
+		return
+	}
+
+	signedTolls, ok := hashmap[h]
+	if !ok {
+		return
+	}
+	var newSignedToll []*SignedToll
+	for _, st := range signedTolls {
+		if st.Toll.Id == toll.Id && st.Site == site {
+			continue
+		}
+		fmt.Println(st.Toll.Id, toll.Id, st.Site, site)
+		newSignedToll = append(newSignedToll, st)
+	}
+	hashmap[h] = newSignedToll
+}
+
+func narrowtomap(latitude, longitude float64, toll *Toll, site int, country string) {
+	h, box := geohash.Encode(latitude, longitude, hashprecision)
+	hashmap, ok := countryMap[country]
+	if !ok {
+		countryMap[country] = make(geoHashMap)
+		hashmap, _ = countryMap[country]
+	}
+	signedToll := &SignedToll{
+		Toll: toll,
+		Site: site,
+		Box:  box,
+	}
+	hashmap[h] = append(hashmap[h], signedToll)
+}
+
+func tomap(latitude, longitude float64, toll *Toll, site int, country string) {
 
 	hashBox := geohash.GetNearGeoHash(latitude, longitude, precision, hashprecision)
+	hashmap, ok := countryMap[country]
+	if !ok {
+		countryMap[country] = make(geoHashMap)
+		hashmap, _ = countryMap[country]
+	}
 
 	for _, hb := range hashBox {
 		signedToll := &SignedToll{
@@ -179,11 +320,11 @@ func Tomap(latitude, longitude float64, toll *Toll, site int) {
 			Site: site,
 			Box:  hb.Box,
 		}
-		globalGeoHashMap[hb.Hash] = append(globalGeoHashMap[hb.Hash], signedToll)
+		hashmap[hb.Hash] = append(hashmap[hb.Hash], signedToll)
 	}
 }
 
-func (oriToll *OriToll) ToToll() *Toll {
+func (oriToll *oriToll) ToToll() *Toll {
 	res := &Toll{
 		Id:    oriToll.Id,
 		Price: oriToll.Price,
